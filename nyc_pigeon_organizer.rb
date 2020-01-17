7a9aa7527cda79ced44712ed18ed7c834afa2f2b
Francis Michael Maggio <bluebassist2002@yahoo.com>
2020-01-17 21:23:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+
+# create names hash with empty values
+
+name_array = data[:gender].map { |k, v| v}
+consolidated_name_array = name_array[0] + name_array[1]  
+pigeon_names_hash = Hash[consolidated_name_array.collect { |item| [item, ""] } ]
+
+#modify names hash to include color, gender and lives
+
+  pigeon_names_hash.each do |key, value|
+    pigeon_names_hash[key] = {:color => [], :gender => [], :lives => [] }
+  end
+
+
+
+
+
+
+# color
+
+# gender
+
+# living
+
+
+pigeon_names_hash
 end

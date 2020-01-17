edf7cde28863c1fd34a969bd98bb6fa52ff09709
Francis Michael Maggio <bluebassist2002@yahoo.com>
2020-01-17 21:26:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,24 +8,14 @@ pigeon_names_hash = Hash[consolidated_name_array.collect { |item| [item, ""] } ]
 
 #modify names hash to include color, gender and lives
 
-  pigeon_names_hash.each do |key, value|
+pigeon_names_hash.each do |key, value|
     pigeon_names_hash[key] = {:color => [], :gender => [], :lives => [] }
   end
 
-
-name_array = pigeon_data[:gender].map { |k, v| v}
-consolidated_name_array = name_array[0] + name_array[1]  
-pigeon_names_hash = Hash[consolidated_name_array.collect { |item| [item, ""] } ]
-
-pigeon_names_hash.each do |key, value|
-  pigeon_names_hash[key] = {:color => [], :gender => [], :lives => [] }
-  end
-  
-
-
-
 #external loop through each name
+
 consolidated_name_array.each do |name|
+
 #internal loop through each color
   pigeon_data[:color].reduce({}) do |memo, pair|
     color_symbol = pair[0]
